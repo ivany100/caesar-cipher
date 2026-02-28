d46550e14069d3760da3cf3a1b0980b16e4d4e21
Ivan Yurchak <ivanyurchak3@gmail.com>
2026-02-28 22:58:14 +0100

    Add rubocop

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,30 +2,26 @@
 # a = 97, z = 122
 
 def caesar_cipher(string, shift)
-  shift = shift % 26
+  shift %= 26
 
   chars_array = string.bytes
-  shifted_string = ""
+  shifted_string = ''
 
   chars_array.each do |char|
     shifted_char = char
 
     if 65 <= char && char <= 90
       shifted_char += shift
-      if shifted_char > 90
-        shifted_char -= 26
-      end
+      shifted_char -= 26 if shifted_char > 90
     elsif 97 <= char && char <= 122
       shifted_char += shift
-      if shifted_char > 122
-        shifted_char -= 26
-      end
+      shifted_char -= 26 if shifted_char > 122
     end
 
     shifted_string << shifted_char.chr
   end
-  
+
   shifted_string
 end
 
-puts caesar_cipher("What a string!", 5)
+puts caesar_cipher('What a string!', 5)
